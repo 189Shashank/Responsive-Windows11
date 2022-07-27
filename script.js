bad4c6b244cb10d3df76db32c3f911aa9483fbe8
Shashank <shashanks2754@gmail.com>
2022-07-27 08:06:20 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log("SHASHANK");
+let para=document.getElementById('para');
+document.addEventListener('mouseenter',function(){
+  let p=para.innerText;
+    console.log(p);
+    console.log("sss");
+   
+})
